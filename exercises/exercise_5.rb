e8fd53802ac7b4fc1db02a74c103a4963d0bb87a
Evgheni Barducov <52833790+baovevni@users.noreply.github.com>
2024-06-24 20:03:54 -0400

    Implement exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,10 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+total_revenue = Store.sum(:annual_revenue)
+puts "Total revenue: #{total_revenue}"
+average_revenue = Store.average(:annual_revenue)
+puts "Average revenue: #{average_revenue}"
+
+best_stores = Store.where("annual_revenue < ?", 1000000).count
+puts "Number of stores generating $1M or more in annual sales: #{best_stores}"
